c7f74f03d6d6d5ca0e3046e714fc6d8124770e9b
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2022-12-22 18:01:43 +0100

    completate le milestone. da sistemare come vengono visualizzate le immagini

--- script.js
+++ script.js
@@ -21,3 +21,99 @@ const data = [
       text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
   ];
+
+//Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+//Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile assieme al suo titolo e testo.
+
+
+//seleziono gli elementi che dovro targtettizzare 
+
+let bigImmagine = document.querySelector(".immagine");
+let smallImmagini = document.querySelector(".carosello");
+let arrowUp = document.getElementById("prec");
+let arrowDown = document.getElementById("next");
+let messaggio = "active";
+
+//la vado a riempire con tutte le foto la sezione grande
+
+for(let i = 0 ; i < data.length ; i++){
+  //mi seleziono i valori di ogni oggetto 
+  
+  let url = data[i].image;
+  let titolo = data[i].title;
+  let text = data[i].text;
+  
+  //li unisco alla sezione immagine
+  if(i != 0){
+    messaggio ="";
+  }
+
+  bigImmagine.innerHTML += `
+  <img id="${i}"src="${url}" alt="immagine" class="${messaggio}"  >
+  <div class="testo ${messaggio}" id="${i} testo">
+    <h4>${titolo}</h4>
+    <p>${text}</p>
+  </div>`
+  
+
+}
+
+
+
+
+//funzione per rimuovere la classe active
+
+
+
+function removeActive (x){
+  //voglio che dandogli l'indice della classe active mi rimuova la classe active
+  //seleziono la classe active
+  let attiva = document.getElementById(x); //dove x è l'idice della classe attiva
+  attiva.classList.remove("active");
+  
+  let attivaTesto = document.getElementById(`${x} testo`);
+  attivaTesto.classList.remove("active");
+  
+}
+
+//funzione per aggiungere la classe active
+function addActive (x){
+  let attiva = document.getElementById(x); //dove x è l'idice della classe attiva
+  attiva.classList.add("active");
+  
+  let attivaTesto = document.getElementById(`${x} testo`);
+  attivaTesto.classList.add("active");
+}
+
+//creo una variabile per targhettizzare la foto active
+let contatore = 0 ;
+
+
+function picAfter (){
+  //quando ci clicco voglio che l'immagine attiva non lo sia più
+  removeActive(contatore);
+  //aumento il contatore di 1 
+  contatore += 1;
+  addActive(contatore);
+  //voglio rendere attiva l'immagine con il nuovo contatore
+}
+
+arrowUp.addEventListener("click", function(){
+  removeActive(contatore);
+  if(contatore == 0){
+    contatore = 4;
+  }else{
+    contatore -= 1;
+  }
+  addActive (contatore);
+});
+arrowDown.addEventListener("click", function(){
+  removeActive(contatore);
+  if(contatore == 4){
+    contatore = 0;
+  }else{
+    contatore += 1;
+  }
+  addActive (contatore);
+});
+console.log(bigImmagine);
